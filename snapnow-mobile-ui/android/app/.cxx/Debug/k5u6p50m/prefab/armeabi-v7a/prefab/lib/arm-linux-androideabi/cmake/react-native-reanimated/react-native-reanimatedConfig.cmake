ddb230bcc1e5aec3c0c9953d44668f789c53ffde
haphu2512-java <haxuanphudeptrai123@gmail.com>
2025-10-26 18:54:18 +0700

    refactor Project

--- snapnow-mobile-ui/android/app/.cxx/Debug/k5u6p50m/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-reanimated::reanimated)
-add_library(react-native-reanimated::reanimated SHARED IMPORTED)
-set_target_properties(react-native-reanimated::reanimated PROPERTIES
-    IMPORTED_LOCATION "E:/1.HK1_2025/ReactNative/deTai/snapnow-social-media-app/snapnow-mobile-ui/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/20o2k736/obj/armeabi-v7a/libreanimated.so"
-    INTERFACE_INCLUDE_DIRECTORIES "E:/1.HK1_2025/ReactNative/deTai/snapnow-social-media-app/snapnow-mobile-ui/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- snapnow-mobile-ui/android/app/.cxx/Debug/k5u6p50m/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "C:/Users/haxua/.gradle/caches/8.14.3/transforms/a245e1a7ebe96402517c2149f4f499da/transformed/hermes-android-0.81.4-debug/prefab/modules/libhermes/libs/android.x86_64/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/haxua/.gradle/caches/8.14.3/transforms/a245e1a7ebe96402517c2149f4f499da/transformed/hermes-android-0.81.4-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
